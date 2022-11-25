c0c8fede6ee386182cfd95a4014c6b990b91e3f8
Jay Yang <jy09567824@gmail.com>
2022-11-26 02:41:07 +0800

    testMsg again

--- index.js
+++ index.js
@@ -126,7 +126,46 @@ const listMsgCH = {
     "paddingBottom": "none"
   }
 }
-const testMsg = { type: 'text', text: event.message.text }
+const testMsg = {
+  "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Header text"
+      }
+    ]
+  },
+  "hero": {
+    "type": "image",
+    "url": "https://example.com/flex/images/image.jpg"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Body text"
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Footer text"
+      }
+    ]
+  },
+  "styles": {
+    "comment": "See the example of a bubble style object"
+  }
+}
 
   if (event.message.text == "餐廳") {
     return client.replyMessage(event.replyToken, testMsg);
