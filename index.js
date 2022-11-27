1d508eecd67e48d11f0d62f88018ee455f8dadc1
Jay Yang <jy09567824@gmail.com>
2022-11-27 14:25:58 +0800

    test data format

--- index.js
+++ index.js
@@ -106,7 +106,106 @@ const restaurantTypeMsgCH = {
     },
   },
 }
-const beforeCarouselMsg = { type: 'text', text: `以下為您推薦三間${this.event.message.text}餐廳，祝您享用愉快、有個美好的一天！
+const restaurantTypeMsgEN = {
+  type: "flex",
+  altText: "Please choose your 請選擇您要的餐廳種類：中式餐廳、日式餐廳、西式⋯⋯",
+  contents: {
+    type: "bubble",
+    hero: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "text",
+          text: "請點選您要的餐廳種類",
+          style: "normal",
+          decoration: "none",
+          align: "center",
+          weight: "bold",
+          size: "lg",
+          margin: "xxl",
+        },
+      ],
+      backgroundColor: "#f7f4ef"
+    },
+    body: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "box",
+          layout: "horizontal",
+          contents: [
+            {
+              type: "image",
+              url: "https://app-line-bot-foodie.herokuapp.com/images/list_ch_001.png",
+              action: {
+                type: "message",
+                label: "action",
+                text: "中式餐廳",
+              },
+            },
+            {
+              type: "image",
+              url: "https://app-line-bot-foodie.herokuapp.com/images/list_ch_002.png",
+              action: {
+                type: "message",
+                label: "action",
+                text: "日式餐廳",
+              },
+            },
+            {
+              type: "image",
+              url: "https://app-line-bot-foodie.herokuapp.com/images/list_ch_003.png",
+              action: {
+                type: "message",
+                label: "action",
+                text: "西式餐廳",
+              },
+            },
+          ],
+        },
+        {
+          type: "box",
+          layout: "horizontal",
+          contents: [
+            {
+              type: "image",
+              url: "https://app-line-bot-foodie.herokuapp.com/images/list_ch_004.png",
+              action: {
+                type: "message",
+                label: "action",
+                text: "咖啡廳",
+              },
+            },
+            {
+              type: "image",
+              url: "https://app-line-bot-foodie.herokuapp.com/images/list_ch_005.png",
+              action: {
+                type: "message",
+                label: "action",
+                text: "美式餐廳",
+              },
+            },
+            {
+              type: "image",
+              url: "https://app-line-bot-foodie.herokuapp.com/images/list_ch_006.png",
+              action: {
+                type: "message",
+                label: "action",
+                text: "異國風味",
+              },
+            },
+          ],
+        },
+      ],
+      paddingTop: "none",
+      paddingBottom: "none",
+      backgroundColor: "#f7f4ef"
+    },
+  },
+}
+const beforeCarouselMsg = { type: 'text', text: `以下為您推薦三間餐廳，祝您享用愉快、有個美好的一天！
 提示：點擊圖片查看店家資訊
 
 Here are the 3 options for you.
